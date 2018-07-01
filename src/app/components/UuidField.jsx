bd6d53f2985845ef7aed0ed60d9196c46f32fe13
thefringeninja <joao.paul@braganca.name>
2018-07-01 15:33:44 +0200

    bypassing validation check on change for uuid

--- src/app/components/UuidField.jsx
+++ src/app/components/UuidField.jsx
@@ -3,42 +3,36 @@ import {
     Button, 
     TextField
 } from '@material-ui/core';
+import {
+    SpaceBar
+} from '@material-ui/icons'
 import uuid from 'uuid';
 import { ComposedComponent } from 'react-schema-form';
 
 class UuidField extends React.PureComponent {
-    constructor(props) {
-        super(props);
-
-        const { model, form, value, classes } = this.props;
-        const { key } = form;
-    }
-
-    _onNewUuidClick = e => this.props.onChangeValidate({
-        ...e,
-        target: {
-            ...e.target,
-            value: uuid.v4()
-        }
-    });
+    _onNewUuidClick = () => this.props.onChange(this.props.form.key, uuid.v4());
 
     render() {
         return (
             <div className={this.props.form.htmlClass}>
-                <TextField
+                <TextField                    
                     type={this.props.form.type}
                     label={this.props.form.title}
                     error={this.props.error || this.props.errorText}
                     onChange={this.props.onChangeValidate}
                     defaultValue={this.props.value}
-                    value={this.props.value}
                     disabled={this.props.form.readonly}
                     helperText={this.props.error ? this.props.errorText : this.props.hintText}
                     style={this.props.form.style || {width: '100%'}}
                 />
                 <Button
+                    color='primary'
+                    variant={'contained'}
                     onClick={this._onNewUuidClick}
-                />
+                >
+                    New UUID
+                    <SpaceBar />
+                </Button>
             </div>);
     }
 }
