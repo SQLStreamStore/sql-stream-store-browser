032c4b653543dd8ff54c713ebfd71b99523ce5ea
thefringeninja <joao.paul@braganca.name>
2018-07-13 01:47:48 +0200

    generic fallback component if we don't recognize the rel

--- src/stream-store/views/index.js
+++ src/stream-store/views/index.js
@@ -2,11 +2,13 @@ import Home from './Home';
 import Stream from './Stream';
 import StreamMessage from './StreamMessage';
 import StreamMetadata from './StreamMetadata';
+import Unknown from './Unknown';
 import rels from '../rels';
 
 export default {
     [rels.feed]: Stream,
-    [rels.message]: StreamMessage,
+    //[rels.message]: StreamMessage,
     [rels.index]: Home,
     [rels.metadata]: StreamMetadata,
+    "_unknown": Unknown
 };
