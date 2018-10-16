83190d74437575f28e9e83ac46417c79f3cf4dbd
thefringeninja <joao.paul@braganca.name>
2018-10-16 12:27:19 +1000

    remove jiggling when progress bar shows

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { LinearProgress, Modal } from '@material-ui/core';
 
-const Loading = ({ open }) =>
-    open ? (
-        <div>
-            <LinearProgress variant={'indeterminate'} />
-            <Modal disableBackdropClick disableEscapeKeyDown open>
-                <span />
-            </Modal>
-        </div>
-    ) : null;
+const variants = {
+    true: 'indeterminate',
+    false: 'determinate',
+};
 
+const values = {
+    true: undefined,
+    false: 0,
+};
+
+const Loading = ({ open }) => (
+    <div>
+        <LinearProgress variant={variants[open]} value={values[open]} />
+        <Modal disableBackdropClick disableEscapeKeyDown open={open}>
+            <span />
+        </Modal>
+    </div>
+);
 export default Loading;
