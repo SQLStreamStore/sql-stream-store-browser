5bbd5ed04063bcfedad92d8ff27aa5839273914d
thefringeninja <joao.paul@braganca.name>
2018-10-28 17:57:15 +1000

    ok guess it didn't like these exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,13 @@
-export {
-    default as AuthorizationProvider,
+import AuthorizationProvider, {
     withAuthorization,
 } from './AuthorizationProvider';
+import NavigationProvider, { withNavigation } from './NavigationProvider';
+export { AuthorizationProvider, withAuthorization };
 export { default as Hyperlink } from './Hyperlink';
 export { default as HyperMediaControls } from './HyperMediaControls';
 export { default as Loading } from './Loading';
 export { default as NavigationLinks } from './NavigationLinks';
-export {
-    default as NavigationProvider,
-    withNavigation,
-} from './NavigationProvider';
+export { NavigationProvider, withNavigation };
 export { default as Notifications } from './Notifications';
 export { default as StreamBrowser } from './StreamBrowser';
 export { default as mount } from './mount';
