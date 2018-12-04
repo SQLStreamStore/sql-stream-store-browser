9ea87d44cedc9615a49b5df89ab66f8e2db847ab
Jthefringeninja <joao.paul@braganca.name>
2018-12-04 15:15:35 +0100

    fixed ugly header alignement

--- src/stream-store/Viewer/HalViewer/components/StreamHeader.tsx
+++ src/stream-store/Viewer/HalViewer/components/StreamHeader.tsx
@@ -1,17 +1,19 @@
 import { Table } from 'components';
-import React from 'react';
+import React, { CSSProperties } from 'react';
+
+const nowrap: CSSProperties = { whiteSpace: 'nowrap' };
 
 const StreamHeader = () => (
     <Table.Head>
         <Table.Row>
-            <Table.Cell>{'StreamId'}</Table.Cell>
-            <Table.Cell>{'Message Id'}</Table.Cell>
-            <Table.Cell>{'Created UTC'}</Table.Cell>
-            <Table.Cell>{'Type'}</Table.Cell>
+            <Table.Cell style={nowrap}>{'Stream Id'}</Table.Cell>
+            <Table.Cell style={nowrap}>{'Message Id'}</Table.Cell>
+            <Table.Cell style={nowrap}>{'Created UTC'}</Table.Cell>
+            <Table.Cell style={nowrap}>{'Type'}</Table.Cell>
             <Table.Cell style={{ width: '100%' }}>
                 {'Stream Id@Version'}
             </Table.Cell>
-            <Table.Cell>{'Position'}</Table.Cell>
+            <Table.Cell style={nowrap}>{'Position'}</Table.Cell>
         </Table.Row>
     </Table.Head>
 );
